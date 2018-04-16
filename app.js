2590369ade5605c3b87a447aa56e6759d442bc83
a1000891 <Ru$43876>
2018-04-16 17:33:05 +0530

    Swagger Implementation

--- app.js
+++ app.js
@@ -7,12 +7,32 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var cors = require(NODE_PATH + 'cors');
 var compress = require(NODE_PATH + 'compression');
+var swaggerJSDoc = require('swagger-jsdoc');
 
 var app = express();
 app.use(compress());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+var swaggerDefinition = {
+	info: {
+		title: 'Node Server',
+		version: '1.0.0',
+		description: 'APIs Published By Node Server',
+	},
+	host: 'localhost:'+config.serverConfig.port,
+	basePath: '/',
+};
+var options = {
+	swaggerDefinition: swaggerDefinition,
+	apis: ['./server/user/index.js']
+};
+var swaggerSpec = swaggerJSDoc(options);
+app.get('/swagger.json', function(request, response) {
+	response.setHeader('Content-Type', 'application/json');
+	response.send(swaggerSpec);
+});
 
 var route = require('./server/routes/routes.js');
 route.apis(app);
--- server/user/index.js
+++ server/user/index.js
@@ -1,11 +1,146 @@
 var logRequest = require('../utils/log_req');
 var user = require('./controllers/index');
 
+/**
+ * @swagger
+ * definition:
+ *   users:
+ *     properties:
+ *       userId:
+ *         type: string
+ *       userName:
+ *         type: string
+ *       password:
+ *         type: string
+ *       mobileNumber:
+ *         type: string
+ *       emailId:
+ *         type: string
+ *       status:
+ *         type: string
+ *       createAt:
+ *         type: string
+ *         format: date
+ *       updateAt:
+ *         type: string
+ *         format: date
+ */
+
 module.exports = function(app) {
+	/**
+	 * @swagger
+	 * /user:
+	 *   post:
+	 *     tags:
+	 *       - users
+	 *     description: Creates a New User
+	 *     produces:
+	 *       - application/json
+	 *     parameters:
+	 *       - name: User
+	 *         description: User Object
+	 *         in: body
+	 *         required: true
+	 *         schema:
+	 *           $ref: '#/definitions/users'
+	 *     responses:
+	 *       200:
+	 *         description: Successfully created
+	 */
 	app.post("/user", logRequest, user.addUser);
+
+	/**
+	 * @swagger
+	 * /user:
+	 *   get:
+	 *     tags:
+	 *       - users
+	 *     description: Returns Users By Property
+	 *     produces:
+	 *       - application/json
+	 *     responses:
+	 *       200:
+	 *         description: An Array of Users
+	 *         schema:
+	 *           $ref: '#/definitions/users'
+	 */
 	app.get("/user", logRequest, user.findUserByProperty);
+
+	/**
+	 * @swagger
+	 * /user/{userName}:
+	 *   get:
+	 *     tags:
+	 *       - users
+	 *     description: Returns All Users
+	 *     produces:
+	 *       - application/json
+	 *     parameters:
+	 *       - name: userName
+	 *         description: User's userName
+	 *         in: path
+	 *         required: true
+	 *         type: string
+	 *     responses:
+	 *       200:
+	 *         description: Successfully Find
+	 */
 	app.get("/user/:userName", logRequest, user.findUserByUserName);
+
+	/**
+	 * @swagger
+	 * /user:
+	 *   get:
+	 *     tags:
+	 *       - users
+	 *     description: Returns All Users
+	 *     produces:
+	 *       - application/json
+	 *     responses:
+	 *       200:
+	 *         description: An Array of Users
+	 *         schema:
+	 *           $ref: '#/definitions/users'
+	 */
 	app.get("/userList", logRequest, user.findUserList);
+
+	/**
+	 * @swagger
+	 * /user/:
+	 *   put:
+	 *     tags:
+	 *     	 - users
+	 *     description: Updates a Single User
+	 *     produces: application/json
+	 *     parameters:
+	 *       - name: User
+	 *         description: Fields for the User Resource
+	 *         schema:
+	 *           $ref: '#/definitions/user'
+	 *     responses:
+	 *       200:
+	 *         description: Successfully updated
+	 */
 	app.put("/user", logRequest, user.updateUser);
+
+	/**
+	 * @swagger
+	 * /user/:
+	 *   delete:
+	 *     tags:
+	 *       - users
+	 *     description: Deletes a Single User
+	 *     produces:
+	 *       - application/json
+	 *     parameters:
+	 *       - name: userName
+	 *         description: User's userName
+	 *         in: path
+	 *         required: true
+	 *         type: string
+	 *     responses:
+	 *       200:
+	 *         description: Successfully deleted
+	 */
 	app.delete("/user", logRequest, user.deleteUser);
 }
